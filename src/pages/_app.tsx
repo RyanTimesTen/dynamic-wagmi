92b2f4295ba0b1e2df5455fabb9a72c5ef1ac876
Ryan Gilbert <ryangilbert7926@gmail.com>
2022-12-05 13:59:00 -0500

    feat: wagmi dynamic connector

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,33 @@
-import type { AppProps } from 'next/app'
-import NextHead from 'next/head'
-import * as React from 'react'
-import { WagmiConfig } from 'wagmi'
+import type { AppProps } from 'next/app';
+import NextHead from 'next/head';
+import * as React from 'react';
+import { WagmiConfig } from 'wagmi';
+import { DynamicContextProvider } from '@dynamic-labs/sdk-react';
+import { ConnectDynamicWagmi } from '@dynamic-labs/wagmi-connector';
 
-import { client } from '../wagmi'
+import { client } from '../wagmi';
 
 function App({ Component, pageProps }: AppProps) {
-  const [mounted, setMounted] = React.useState(false)
-  React.useEffect(() => setMounted(true), [])
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => setMounted(true), []);
   return (
-    <WagmiConfig client={client}>
-      <NextHead>
-        <title>wagmi</title>
-      </NextHead>
+    <DynamicContextProvider
+      settings={{
+        environmentId: '67ddb74b-e30f-4039-9a25-f033c79f1207',
+        multiWallet: true,
+      }}
+    >
+      <WagmiConfig client={client}>
+        <ConnectDynamicWagmi>
+          <NextHead>
+            <title>wagmi</title>
+          </NextHead>
 
-      {mounted && <Component {...pageProps} />}
-    </WagmiConfig>
-  )
+          {mounted && <Component {...pageProps} />}
+        </ConnectDynamicWagmi>
+      </WagmiConfig>
+    </DynamicContextProvider>
+  );
 }
 
-export default App
+export default App;
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,46 +1,12 @@
-import { chain, configureChains, createClient } from 'wagmi'
+import { configureChains, createClient, defaultChains } from 'wagmi';
 
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { publicProvider } from 'wagmi/providers/public';
 
-import { publicProvider } from 'wagmi/providers/public'
+import { dynamicWagmiConnector } from '@dynamic-labs/wagmi-connector';
 
-const { chains, provider, webSocketProvider } = configureChains(
-  [
-    chain.mainnet,
-    ...(process.env.NODE_ENV === 'development' ? [chain.goerli] : []),
-  ],
-  [
-    publicProvider(),
-  ],
-)
+const { provider } = configureChains(defaultChains, [publicProvider()]);
 
 export const client = createClient({
-  autoConnect: true,
-  connectors: [
-    new MetaMaskConnector({ chains }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: 'wagmi',
-      },
-    }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        qrcode: true,
-      },
-    }),
-    new InjectedConnector({
-      chains,
-      options: {
-        name: 'Injected',
-        shimDisconnect: true,
-      },
-    }),
-  ],
+  connectors: [dynamicWagmiConnector],
   provider,
-  webSocketProvider,
-})
+});
